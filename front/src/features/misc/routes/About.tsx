66c244e8632554c5ea2d156102fc669cd6fe0ea7
rtokkie <natto.and.eggs@gmail.com>
2022-11-03 13:27:27 +0900

    b

--- front/src/features/misc/routes/About.tsx
+++ front/src/features/misc/routes/About.tsx
@@ -1,6 +1,11 @@
+import { useEffect } from 'react';
+
 import { AppLayout } from '@/components/AppLayout';
 
 export const About = () => {
+  useEffect(() => {
+    fetch('http://127.0.0.1:8080').then(console.log);
+  }, []);
   return (
     <AppLayout>
       <div>About</div>
