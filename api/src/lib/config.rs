50033a446837dfd189076c7ff5231e35c1c1aaba
rtokkie <natto.and.eggs@gmail.com>
2022-11-05 23:05:07 +0900

    h

---
+++ api/src/lib/config.rs
@@ -0,0 +1,33 @@
+use lazy_static::lazy_static;
+use serde::Deserialize;
+
+lazy_static! {
+    pub static ref CONFIG: Config = Config::new().unwrap();
+}
+
+#[derive(Deserialize)]
+pub struct Config {
+    pub host: String,
+    pub port: String,
+    pub database_url: String,
+    pub frontend_origins: Vec<String>,
+}
+
+impl Config {
+    fn new() -> Result<Self, config::ConfigError> {
+        let frontend_origins: Vec<String> = std::env::vars()
+            .into_iter()
+            .filter(|v| v.0.starts_with("FRONTEND_ORIGIN_"))
+            .map(|v| v.1)
+            .collect();
+
+        let environment = config::Environment::default().try_parsing(true);
+        let config = config::Config::builder()
+            .set_default("host", "127.0.0.1")?
+            .set_default("port", "9099")?
+            .set_default("frontend_origins", frontend_origins)?
+            .add_source(environment)
+            .build()?;
+        config.try_deserialize()
+    }
+}
---
+++ api/src/main.rs
@@ -0,0 +1,29 @@
+mod lib;
+
+use crate::lib::{config::CONFIG, cors::cors};
+
+use actix_web::{get, middleware::Logger, App, HttpServer, Responder};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    if cfg!(debug_assertion) {
+        dotenv::dotenv().ok();
+    }
+
+    env_logger::init_from_env(env_logger::Env::default().default_filter_or("into"));
+
+    HttpServer::new(move || {
+        App::new()
+            .wrap(Logger::default())
+            .wrap(cors())
+            .service(index)
+    })
+    .bind(format!("{}:{}", CONFIG.host, CONFIG.port))?
+    .run()
+    .await
+}
+
+#[get("/")]
+async fn index() -> impl Responder {
+    format!("HELLO WORLD!")
+}
