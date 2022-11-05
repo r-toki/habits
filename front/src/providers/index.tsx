9e7af23132bfde052010cd57c17a049eac352a62
rtokkie <natto.and.eggs@gmail.com>
2022-11-05 18:18:16 +0900

    f

--- front/src/providers/index.tsx
+++ front/src/providers/index.tsx
@@ -1,6 +1,11 @@
+import { ChakraProvider } from '@chakra-ui/react';
 import { ReactNode } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
 export const AppProvider = ({ children }: { children: ReactNode }) => {
-  return <BrowserRouter>{children}</BrowserRouter>;
+  return (
+    <BrowserRouter>
+      <ChakraProvider>{children}</ChakraProvider>
+    </BrowserRouter>
+  );
 };
