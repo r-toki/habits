10bd719b0fb72de0190fccb52333595b5360d648
rtokkie <natto.and.eggs@gmail.com>
2022-11-28 01:32:01 +0900

    w

--- back/src/model/query/daily_record.rs
+++ back/src/model/query/daily_record.rs
@@ -86,8 +86,8 @@ pub async fn find_daily_record(
                 r#"
                 select archived_at is not null "archived!", id, name from habits
                 where user_id = $1
-                and (archived_at is null or archived_at > ($2::date)::timestamp at time zone 'Asia/Tokyo')
-                and created_at < ($2::date)::timestamp at time zone 'Asia/Tokyo' + interval '1 day'
+                and (archived_at is null or archived_at > ($2::date at time zone 'Asia/Tokyo')::timestamp)
+                and created_at < ($2::date at time zone 'Asia/Tokyo')::timestamp + interval '1 day'
                 order by sort_number
                 "#,
                 user_id.clone(),
